251177a920343df43caef9ac64a1e6a1e671a476
Oshada1988 <lahirupr471@gmail.com>
2021-07-19 15:20:53 +0530

    complete login form front end development

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -11,25 +11,32 @@ export default function Loggin() {
     const[email,setEmail] = useState("");
     const [password, setpassword] = useState("");
 
-   async function register(){
+   async function login(){
        
     if(email.length == 0){
-
-    }else if(password.length == 0){
-
-    }
-
+        document.getElementById('mail_error').style.display = "block";
+        // document.getElementById('password_error').style.display = "none";
+        document.getElementById('exists').style.display = "none";
+    }else{
         try{
             const loginData = {
                 email,
                 password,
             }
 
-            await axios.post("http://localhost:5000/user/login",loginData);
+            await axios.post("http://localhost:5000/user/login",loginData).then(()=>{
+
+            }).catch(()=>{
+                document.getElementById('exists').style.display = "block";
+                document.getElementById('mail_error').style.display = "none";
+            })
 
         }catch(err){
             console.error(err);
         }
+    }
+
+        
     }
 
 
@@ -48,12 +55,12 @@ export default function Loggin() {
                     
                     <input type="mail" className="form-control" name="Email" placeholder="Enter your E-mail address"
                     onChange={(e)=> setEmail(e.target.value)} value={email} required/>
-                    <div id="id_error" style={{ display: "none", color:"red", marginLeft:0,marginTop:-5 }}>contract is required</div><br />
+                   <div id="mail_error" style={{ display: "none", color:"red", marginLeft:0,marginTop:-5 }}>please enter email address</div><br />
                     <input type="password" className="form-control" name="password" placeholder="Enter your password"
                      onChange={(e)=> setpassword(e.target.value)} required/>
-                     <div id="id_error" style={{ display: "none", color:"red", marginLeft:0,marginTop:-5 }}>contract is required</div><br />
-                    
-                    <a href="#"><div className="btn btn-info">log in</div></a>
+                      {/* <div id="password_error" style={{ display: "none", color:"red", marginLeft:0,marginTop:-5 }}>please enter password</div><br /> */}
+                      <div id="exists" style={{ display: "none", color:"red", marginLeft:0,marginTop:-5 }}>mail address or password is invalid</div><br />
+                    <a onClick={e=>{login()}} ><div className="btn btn-info">log in</div></a>
                     <a href="#"><div className="btn btn-info1">Forgot password?</div></a>
                    <h3 className= "register">I haven't an account?<Link to="/register" className="regLink"> Register</Link></h3>
                    
