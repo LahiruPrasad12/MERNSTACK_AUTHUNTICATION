0c4da745ee933c1e3d023ad0d05860a2171302c9
Oshada1988 <lahirupr471@gmail.com>
2021-07-21 11:03:56 +0530

    create add todo frontend part

--- frontend/src/components/layout/Home.js
+++ frontend/src/components/layout/Home.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React,{ useState} from 'react'
 import "./SideNav.css"
 import "./Home.css"
 import image1 from "../images/logo2.png"
 import Loggin from '../auth/Login'
 
 export default function Home() {
+    const [subject, setsubject] = useState("");
+    const [priority, setpriority] = useState("");
+    const [endDate, setendDate] = useState();
+    const [description, setdescription] = useState("")
+
+    function saveData(e){
+        alert("OK");
+    }
+
+
+
+
     return (
         <div class="container-fluid-side-2">
             <div className="Header">
@@ -12,8 +24,30 @@ export default function Home() {
             </div>
 
             <div className="cont">
-                <input type="text" className="sub" placeholder="Subject"/>
+                <input type="text" className="sub" placeholder="Subject" onChange={(e)=>{setsubject(e.target.value)}} value={subject}/>
+                <h4 className="des1">Select task priority</h4>&nbsp;&nbsp;&nbsp;<h4 className="des2">Select task end date</h4>
+ 
+                <select className="form"
+                onChange={(e)=>{setpriority(e.target.value)}} value={priority}>
+                    <option selected>Open this select menu</option>
+                    <option value="1">High</option>
+                    <option value="2">Midium</option>
+                    <option value="3">Law</option>
+                </select>&nbsp;&nbsp;&nbsp; 
+                <input type="date" className="cal" placeholder="Select Your date" onChange={(e)=>{setendDate(e.target.value)}} value={endDate}/><br></br>
+
+            
+               
+                <input type="text" className="desc" placeholder="Enter Description" onChange={(e)=>{setdescription(e.target.value)}} value={description}/>
+
+               
             </div>
-        </div>
+
+            <button className="ok" onClick={saveData}>Create TO-DO</button>
+
+            
+         </div>
+
+        
     )
 }
