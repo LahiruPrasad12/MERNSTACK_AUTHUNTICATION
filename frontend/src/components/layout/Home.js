2fa10a7d789ebcc52881195760fd716fad489a6c
Oshada1988 <lahirupr471@gmail.com>
2021-07-21 08:47:57 +0530

    install concurrency dependency and run frontend and backend once

--- frontend/src/components/layout/Home.js
+++ frontend/src/components/layout/Home.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import "./SideNav.css"
+import "./Home.css"
 import image1 from "../images/logo2.png"
 import Loggin from '../auth/Login'
 
 export default function Home() {
     return (
         <div class="container-fluid-side-2">
-        <h1>Home</h1>
-        <h2>Home 2</h2>
-     </div>
+            <div className="Header">
+             ADD YOUR TODO HERE
+            </div>
+
+            <div className="cont">
+                <input type="text" className="sub" placeholder="Subject"/>
+            </div>
+        </div>
     )
 }
